ea34d2142e79493af4c0584b7082574defa7feb3
Rajshree Thorat <rthorat@stec-inc.com>
2011-07-07 17:05:53 +0530

    libzpool: Allow pool creation with version
    The patch adds some features to the libzpool library. The
    list of features added to the library are
    
    - setting the zpool version i.e. zpool set version
    - getting the zpool version i.e. zpool get version
    
    Signed-off-by: Shrirang Phansalkar <sphansalkar@stec-inc.com>
    Signed-off-by: Rajshree Thorat <rthorat@stec-inc.com>
    Signed-off-by: Prasad Joshi <pjoshi@stec-inc.com>

--- client/bin/libzpool.py
+++ client/bin/libzpool.py
@@ -1,16 +1,25 @@
-import logging
+import logging, re
 from autotest_lib.client.bin import  utils, configzfs
 from autotest_lib.client.common_lib import error
 from configzfs import *
 
-def create(pool, disks):
+def create(pool, disks, version = 0):
     if pool == "":
        raise error.TestFail("cannot create pool :  Missing pool name..")
     if disks == "":
        raise error.TestFail("cannot create pool : Missing disk list..")
     if pool_exists(pool) == SUCCESS:
-       raise error.TestFail("cannot create '" + pool + "': " + pool + " already exists")
-    utils.system("zpool create " + pool + " " + disks)
+       raise error.TestFail("cannot create '" + pool + "': " + pool + " already
+		       exists")
+    try:
+       status = SUCCESS
+       if version == 0:
+          utils.system_output("zpool create " + pool + " " + disks)
+       else:
+          utils.system("zpool create -o version=" + version + " " + pool + " " +
+                        disks)
+    except:
+       status = FAIL
     return SUCCESS
 
 def pool_exists(pool):
@@ -30,10 +39,30 @@ def destroy(pool):
        utils.system("zpool destroy " + pool)
     return SUCCESS
 
-def add(pool, disk):
+def add_disk(pool, disks):
     if pool == "":
-       raise error.TestFail("cannot add disk : Missing pool name..")
+       raise error.TestFail("cannot add disks : Missing pool name..")
     if disk == "":
-       raise error.TestFail("cannot add disk : Missing disk name..")
-    utils.system("zpool add -f " + pool + " " + disk)
+       raise error.TestFail("cannot add disks : Missing disk name..")
+    utils.system("zpool add -f " + pool + " " + disks)
     return SUCCESS
+
+def set_version(pool, version):
+    if pool == "":
+       raise error.TestFail("cannot set version : Missing pool name..")
+    if version == "":
+       raise error.TestFail("cannot set version : Missing version number..")
+    try:
+       status = SUCCESS
+       utils.system_output("zpool set version=" + str(version) + " " + pool)
+    except:
+       status = FAIL
+    return status
+
+def get_version(pool):
+    if pool == "":
+       raise.error.TestFail("cannot get version : Missing pool name..")
+    version = utils.system_output("zpool get version "+ pool + "| grep version |
+		    awk '{print $3}'")
+    version = re.sub('\n',"",version)
+    return version
