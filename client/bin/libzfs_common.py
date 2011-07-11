0c50dd0b20a974045b28d85027ef42320ee099c4
Prasad Joshi <prasadjoshi124@gmail.com>
2011-07-11 09:06:38 +0100

    libzfs_common: remove uninitialized variable
    Suggested-by: Shrirang Phansalkar <sphansalkar@stec-inc.com>
    Signed-off-by: Prasad Joshi <pjoshi@stec-inc.com>

--- client/bin/libzfs_common.py
+++ client/bin/libzfs_common.py
@@ -6,14 +6,14 @@ from configzfs import *
 
 def load_zfs_modules():
     if utils.load_module("zfs") == False:
-        logging.info(zfs_module + " module has already been loaded..")
+        logging.info("zfs module has already been loaded..")
     else:
-        logging.info(zfs_module + " module loaded successfully..")
+        logging.info("zfs module loaded successfully..")
     return SUCCESS
 
 def unload_zfs_modules():
     if utils.unload_module("zfs") == False:
-        logging.info(zfs_module + " cannot unload module..")
+        logging.info("zfs cannot unload module..")
     else:
-        logging.info(zfs_module + " module unloaded successfully..")
+        logging.info("zfs module unloaded successfully..")
     return SUCCESS
