c64a301ae4d7e6068ac4724b7073b88ce1c6d57f
Rajshree Thorat <rthorat@stec-inc.com>
2011-07-18 15:24:52 +0530

    libzpool: Add necessary features for cachefile script
    The patch adds some features to the libzpool library.
    The list of features added to the library are
    
    - set_property() will set the zpool property.
    - get_property() will give the property value of zpool.
    - import_pool() will import zpool.
    - export_pool() will export zpool.
    
    Signed-off-by: Rajshree Thorat <rthorat@stec-inc.com>
    Signed-off-by: Prasad Joshi <pjoshi@stec-inc.com>

--- client/bin/libzpool.py
+++ client/bin/libzpool.py
@@ -3,24 +3,29 @@ from autotest_lib.client.bin import  utils, configzfs
 from autotest_lib.client.common_lib import error
 from configzfs import *
 
-def create(pool, disks, version = 0):
+def create(pool, disks, version = "", cachefile = "", altroot = ""):
     if pool == "":
        raise error.TestFail("cannot create pool :  Missing pool name..")
     if disks == "":
        raise error.TestFail("cannot create pool : Missing disk list..")
     if pool_exists(pool) == SUCCESS:
-       raise error.TestFail("cannot create '" + pool + "': " + pool + " already
-		       exists")
+       raise error.TestFail("cannot create '" + pool + "': " + pool + " already exists.")
+    options = ""
+    if(version):
+       options = " -o version=" + version
+    if(cachefile):
+       options += " -o cachefile=" + cachefile
+    if(altroot):
+       options += " -o altroot=" + altroot
     try:
        status = SUCCESS
-       if version == 0:
-          utils.system_output("zpool create " + pool + " " + disks)
+       if(options):
+            utils.system_output("zpool create" + options + " " + pool + " " + disks)
        else:
-          utils.system("zpool create -o version=" + version + " " + pool + " " +
-                        disks)
+	    utils.system_output("zpool create "  + pool + " " + disks)
     except:
        status = FAIL
-    return SUCCESS
+    return status
 
 def pool_exists(pool):
     if pool == "":
@@ -47,22 +52,39 @@ def add_disks(pool, disks):
     utils.system("zpool add -f " + pool + " " + disks)
     return SUCCESS
 
-def set_version(pool, version):
+def set_property(pool, pr, val):
     if pool == "":
-       raise error.TestFail("cannot set version : Missing pool name..")
-    if version == "":
-       raise error.TestFail("cannot set version : Missing version number..")
+       raise error.TestFail("cannot set property : Missing pool name..")
+    if pr == "":
+       raise error.TestFail("cannot set property : Missing property name..")
+    if val == "":
+       raise error.TestFail("cannot set property : Missing property value..")
     try:
        status = SUCCESS
-       utils.system_output("zpool set version=" + str(version) + " " + pool)
+       utils.system_output("zpool set " + pr + "=" + val + " " + pool)
     except:
        status = FAIL
     return status
 
-def get_version(pool):
+def get_property(pool, pr):
     if pool == "":
-       raise.error.TestFail("cannot get version : Missing pool name..")
-    version = utils.system_output("zpool get version "+ pool + "| grep version |
-		    awk '{print $3}'")
-    version = re.sub('\n',"",version)
-    return version
+       raise error.TestFail("cannot get property : Missing pool name..")
+    if pr == "":
+       raise error.TestFail("cannot get property : Missing property name..")
+    val = utils.system_output("zpool get " + pr + " " + pool + " | awk '/" + pr +
+		               "/{print $3}'")
+    val = re.sub('\n',"", val)
+    return val
+
+def export_pool(pool):
+    if pool == "":
+       raise error.TestFail("cannot export pool : Missing pool name..")
+    utils.system("zpool export " + pool)
+
+def import_pool(pool, cachefile = ""):
+    if pool == "":
+       raise error.TestFail("cannot export pool : Missing pool name..")
+    if(cachefile):
+       utils.system("zpool import -o cachefile=" + cachefile + " " + pool)
+    else:
+       utils.system("zpool import " + pool)
